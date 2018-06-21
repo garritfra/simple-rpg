7c8584c389f2f8a713c7dfee66e8ea37aa614ceb
garritfra <garritfranke@gmail.com>
2018-06-21 18:29:02 +0200

    add coverage reports

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,8 @@ module.exports = {
   transform: {
     "^.+\\.tsx?$": "ts-jest"
   },
+  collectCoverage: true,
   testRegex: "(/test/.*|(\\.|/)(test|spec))\\.tsx?$",
+  coverageFormats: ["json", "html"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
 };
